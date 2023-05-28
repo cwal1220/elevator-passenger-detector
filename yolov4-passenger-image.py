1f69f1723c05d25f10da8cb1ab9c84594988c8ae
Chan Park <qkrcks1220@naver.com>
2023-05-28 16:46:21 +0900

    [ref] modify resolution

--- yolov4-passenger-image.py
+++ yolov4-passenger-image.py
@@ -1,9 +1,8 @@
 import cv2
 import numpy as np
-# import DeviceManager
+
 
 MAX_PASSENGER_NUM = 9
-
 
 # YOLOv4 모델과 가중치 파일 경로
 model_path = "yolov4-passenger-tiny_last.weights"
@@ -13,6 +12,7 @@ config_path = "yolov4-passenger-tiny.cfg"
 # net = cv2.dnn.readNetFromDarknet(config_path, model_path)
 # or
 net = cv2.dnn.readNet(model_path, config_path)
+
 # net.setPreferableBackend(cv2.dnn.DNN_BACKEND_INFERENCE_ENGINE)
 # net.setPreferableTarget(cv2.dnn.DNN_TARGET_MYRIAD)
 
@@ -20,40 +20,32 @@ net = cv2.dnn.readNet(model_path, config_path)
 # net.setPreferableBackend(cv2.dnn.DNN_BACKEND_CUDA)
 # net.setPreferableTarget(cv2.dnn.DNN_TARGET_CUDA)
 
-# 클래스 이름
+# # 클래스 이름 파일 경로
+# classes_file = "passenger.names"
+
+# # 클래스 이름 로드
+# with open(classes_file, "r") as f:
+#     classes = [line.strip() for line in f.readlines()]
+
 classes = ["passenger"]
 
 # 이미지 파일 경로
-
-# 웹캠으로부터 입력 받기 위해 VideoCapture 객체 생성
-cap = cv2.VideoCapture(0)
-# 웹캠 프레임 사이즈 설정
-cap.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
-cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
-cap.set(cv2.CAP_PROP_BUFFERSIZE, 1)
-
-# frame_width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
-# frame_height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
-
-cv2.namedWindow("YOLOv4-passenger", cv2.WINDOW_NORMAL)
-# cv2.setWindowProperty("YOLOv4-passenger", cv2.WND_PROP_FULLSCREEN, cv2.WINDOW_FULLSCREEN)
+image_path = "test1.jpg"
 
 
-while True:
-    # 웹캠에서 프레임 읽어오기
-    ret, frame = cap.read()
-    if not ret:
-        break
+for i in range(1):
 
-    frame = cv2.resize(frame, (224, 224))
+
+    # 이미지 로드
+    image = cv2.imread(image_path)
 
     # 이미지 크기 가져오기
-    height, width = frame.shape[:2]
+    height, width = image.shape[:2]
 
     # 이미지를 YOLOv4 모델 입력 크기에 맞게 리사이징
     scale = 1 / 255.0
     size = (224, 224)
-    blob = cv2.dnn.blobFromImage(frame, scale, size, swapRB=True, crop=False)
+    blob = cv2.dnn.blobFromImage(image, scale, size, swapRB=True, crop=False)
 
     # YOLOv4 모델에 이미지 입력
     net.setInput(blob)
@@ -73,7 +65,7 @@ while True:
             scores = detection[5:]
             class_id = np.argmax(scores)
             confidence = scores[class_id]
-            if confidence > 0.2:
+            if confidence > 0.5:
                 center_x = int(detection[0] * width)
                 center_y = int(detection[1] * height)
                 w = int(detection[2] * width)
@@ -85,22 +77,19 @@ while True:
                 class_ids.append(class_id)
 
     # NMS(Normalized Maximum Suppression) 적용
-    indexes = cv2.dnn.NMSBoxes(boxes, confidences, 0.2, 0.5)
+    indexes = cv2.dnn.NMSBoxes(boxes, confidences, 0.5, 0.4)
 
-    if len(indexes) > 0:
-        passengerAreaClor = (0, 0, 255)
-        for i in indexes.flatten():
-            x, y, w, h = boxes[i]
-            color = (0, 255, 128)
-            cv2.rectangle(frame, (x, y), (x + w, y + h), color, 2)
-            text = f"{classes[class_ids[i]]}: {confidences[i]:.2f}"
-            cv2.putText(frame, text, (x, y - 5), cv2.FONT_HERSHEY_SIMPLEX, 0.5, color, 2)
+    # 객체 경계 상자 그리기
+    colors = np.random.uniform(0, 255, size=(len(boxes), 3))
+    for i in indexes.flatten():
+        x, y, w, h = boxes[i]
+        color = colors[i]
+        cv2.rectangle(image, (x, y), (x + w, y + h), color, 2)
+        text = f"{classes[class_ids[i]]}: {confidences[i]:.2f}"
+        cv2.putText(image, text, (x, y - 5), cv2.FONT_HERSHEY_SIMPLEX, 0.5, color, 2)
 
-    cv2.putText(frame, "passenger: {}/{}".format(len(indexes), MAX_PASSENGER_NUM), (10, 20), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 255), 2)
+    cv2.putText(image, "passenger: {}/{}".format(len(indexes), MAX_PASSENGER_NUM), (10, 20), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 255), 2)
 
-    # 결과 이미지 출력
-    frame = cv2.resize(frame, (480, 320))
-    cv2.imshow("YOLOv4-passenger", frame)
-    if cv2.waitKey(1) == ord("q"):
-        break
+    # 결과 이미지 저장
+    cv2.imwrite("result.jpg", image)
 
